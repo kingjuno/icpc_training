96c1aa32cd061762469e1eb5eafe4f0070c6b4f3
kingjuno <69108486+kingjuno@users.noreply.github.com>
2020-08-31 11:59:22 +0530

    Create permutation.cpp
    print all permutation of string

---
+++ string/permutation.cpp
@@ -0,0 +1,27 @@
+//here l and r index of string
+// string is permuted from a[l] to a[r]
+//permutation stores all the permuted strings
+
+vector<string>permutation;
+void permute(string a, int l, int r)  
+{  
+    // Base case  
+    if (l == r){ 
+        cout<<a<<endl;
+				permutation.push_back(a);
+		}
+    else
+    {  
+        for (int i = l; i <= r; i++)  
+        {  
+            // Swapping done  
+            swap(a[l], a[i]);  
+  
+            // Recursion called  
+            permute(a, l+1, r);  
+  
+            //backtrack  
+            swap(a[l], a[i]);  
+        }  
+    }  
+}  
